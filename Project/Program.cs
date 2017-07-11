819e0b8e35b4e4068a610f8b74b3aea2f4f26e5f
vittersdesign <vittersdesign@hotmail.com>
2017-07-12 01:08:27 +0200

    C# not done
    Code for reminding me where to place the actual code

---
+++ Project/Program.cs
@@ -0,0 +1,36 @@
+﻿//Not working code is for remind purpose
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+var parser = new FileIniDataParser();
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                IniData data = parser.ReadFile("Configuration.ini");
+                string comPort = data["General"]["COMPort"];
+                SerialPort port = new SerialPort("COMPort",
+                9600, Parity.None, 8, StopBits.One);
+                port.Open();
+                port.Write(args[0]);
+            }
+            else if ([args.object == "mame"])
+            {
+                IniData data = parser.ReadFile("Configuration.ini");
+                string comPort = data["General"]["COMPort"];
+                SerialPort port = new SerialPort("COMPort",
+                9600, Parity.None, 8, StopBits.One);
+                port.Open();
+                port.Write("3");
+            }
+        }
+    }
+}
